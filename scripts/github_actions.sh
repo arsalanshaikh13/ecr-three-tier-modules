6e0a628c5949d27de3b5c9bdd2bb95ff86d22c81
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2026-04-04 16:22:19 +0530

    made some workflow display related code change to github_actions.sh

--- scripts/github_actions.sh
+++ scripts/github_actions.sh
@@ -9,7 +9,7 @@
 # git checkout -b service-discovery
 # git checkout nextjs-fargate
 
-git add .; git commit -m "host network specific changes made"; 
+git add .; git commit -m "made some workflow display related code change to github_actions.sh"; 
 git push;
 # git tag tf-module-ec2-host 
 # git push origin tf-module-ec2-host
@@ -18,38 +18,42 @@ git push;
 # git tag -l "lirw-*" | xargs git tag -d
 
 # Capture current time so we can identify the run we just triggered.
-START_TS="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+# START_TS="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 
-gh workflow run deploy.yml \
-  --ref main \
-  -f build_frontend=false \
-  -f get_frontend=false \
-  -f build_backend=false \
-  -f get_backend=false \
-  -f run_seeding=true
+# gh workflow run deploy.yml \
+#   --ref main \
+#   -f build_frontend=false \
+#   -f get_frontend=false \
+#   -f build_backend=false \
+#   -f get_backend=false \
+#   -f run_seeding=true
 
-# Small delay to allow GitHub to register the new run in run list.
-sleep 3
+# # Small delay to allow GitHub to register the new run in run list.
+# sleep 3
 
-# Fetch both internal run ID and human-readable run number for the latest dispatch after START_TS.
-RUN_ID=$(gh run list \
-  --workflow deploy.yml \
-  --branch main \
-  --event workflow_dispatch \
-  --limit 20 \
-  --json databaseId,number,createdAt \
-  --jq "map(select(.createdAt >= \"$START_TS\")) | first | .databaseId")
+# # Fetch both internal run ID and human-readable run number for the latest dispatch after START_TS.
+# RUN_ID=$(gh run list \
+#   --workflow deploy.yml \
+#   --branch main \
+#   --event workflow_dispatch \
+#   --limit 20 \
+#   --json databaseId,number,createdAt \
+#   --jq "map(select(.createdAt >= \"$START_TS\")) | first | .databaseId")
 
-RUN_NO=$(gh run list \
-  --workflow deploy.yml \
-  --branch main \
-  --event workflow_dispatch \
-  --limit 20 \
-  --json number,createdAt \
-  --jq "map(select(.createdAt >= \"$START_TS\")) | first | .number")
+# RUN_NO=$(gh run list \
+#   --workflow deploy.yml \
+#   --branch main \
+#   --event workflow_dispatch \
+#   --limit 20 \
+#   --json number,createdAt \
+#   --jq "map(select(.createdAt >= \"$START_TS\")) | first | .number")
 
-echo "Triggered deploy workflow: RUN_ID=$RUN_ID RUN_NO=$RUN_NO"
-gh run view "$RUN_ID"
+# echo "Triggered deploy workflow: RUN_ID=$RUN_ID RUN_NO=$RUN_NO"
+# gh run view "$RUN_ID"
+
+# # one line trigger
+# gh run list --workflow deploy.yml --branch main --event workflow_dispatch --limit 1 --json databaseId --jq '.[0].databaseId'
+
 #   # Variables
 # ORG="arsalanshaikh13"
 # REPO="ecr-three-tier"
