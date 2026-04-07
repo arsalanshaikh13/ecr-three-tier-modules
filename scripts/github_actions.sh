e36834a57df338f39b97d171ee74e04607a4adb4
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2026-04-07 16:05:31 +0530

    replace cat << EOF with jq for pomotion.json file to resolve indentiaiton issues

--- scripts/github_actions.sh
+++ scripts/github_actions.sh
@@ -33,7 +33,7 @@ REPO_NAME="ecr-three-tier-modules"
 # pwd
 # SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # ROOT_DIR="${SCRIPT_DIR}/../.github/workflows/promotion.yml"
-git add .; git commit -m "updated promotion.yml used << EOF to resolve indentiaiton issues"
+git add .; git commit -m "replace cat << EOF with jq for pomotion.json file to resolve indentiaiton issues"
 git push ;
 # git tag tf-module-ec2-host-public
 # git push origin tf-module-ec2-host-public
