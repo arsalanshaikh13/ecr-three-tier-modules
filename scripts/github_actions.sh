131851a60e408be90f51c5f2b035c54eb6011c9a
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2026-04-08 13:01:46 +0530

    changed argjson to arg for SUBNET_ID_JSON in ecs-run-task-awsvpc action

--- scripts/github_actions.sh
+++ scripts/github_actions.sh
@@ -34,7 +34,7 @@ REPO_NAME="ecr-three-tier-modules"
 # SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # ROOT_DIR="${SCRIPT_DIR}/../.github/workflows/deploy.yml"
 git add .; 
-git commit -m "removed double quotes around SUBNET_ID_JSON in ecs-run-task-awsvpc action"
+git commit -m "changed argjson to arg for SUBNET_ID_JSON in ecs-run-task-awsvpc action"
 git push  ;
 # git tag tf-module-ec2-host-public
 # git push origin tf-module-ec2-host-public
