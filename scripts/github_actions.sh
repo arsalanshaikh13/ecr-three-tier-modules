a8ff35f964cb6b9d50440adaa172d884283e9371
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2026-04-08 22:56:24 +0530

    in deploy.yml making all probe task to use FARGATE and awsvpc

--- scripts/github_actions.sh
+++ scripts/github_actions.sh
@@ -35,7 +35,7 @@ REPO_NAME="ecr-three-tier-modules"
 # ROOT_DIR="${SCRIPT_DIR}/../.github/workflows/deploy.yml"
 # git status
 git add .; 
-git commit -m "in root/main.tf ecs_ec2 now have probe with Fargate back because ec2 based probe task takes too much time to stop"
+git commit -m "in deploy.yml making all probe task to use FARGATE and awsvpc"
 git push   ;
 # git tag tf-module-ec2-host-public
 # git push origin tf-module-ec2-host-public
